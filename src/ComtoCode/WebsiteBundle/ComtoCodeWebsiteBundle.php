0de9ed84b88a637ce21d2a8a4c45a0240981a454
Nicolas AGUENOT <nicolas@com-to-code.com>
2015-01-13 02:54:11 +0100

    add Com to Code bundle

---
+++ src/ComtoCode/WebsiteBundle/ComtoCodeWebsiteBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ComtoCode\WebsiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ComtoCodeWebsiteBundle extends Bundle
+{
+    protected $name = 'ComtoCodeWebsiteBundle';
+}
